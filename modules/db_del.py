00e963ce12542c3411b4a7f9d4cdb1da6ab59650
genioustar <genioustar@gmail.com>
2018-01-10 15:40:43 +0900

    Create db_del.py
    db 삭제로직 추가

---
+++ modules/db_del.py
@@ -0,0 +1,27 @@
+import pymysql
+import time
+
+cour=None
+conn=None
+
+conn = pymysql.connect(host='211.253.10.91',user='root',password='goehddl',db='haedong4',charset='utf8')
+
+#result = cour.execute('select * from GCG18 where working_day =\'2018-01-09\'')
+with conn.cursor() as cour :
+    result = cour.execute('show tables like "%18%"')
+    result2 = cour.fetchall()
+#print(result)
+#print(result2)
+for i in result2 :
+    query = "select * from %s where working_day =\'2018-01-10\'" % i[0]
+    with conn.cursor() as cour:
+        count = cour.execute(query)
+    if count>0:
+        query1 = "delete from %s where working_day = '2018-01-10'" % i[0]
+        with conn.cursor() as cour:
+            cour.execute(query1)
+            print(query)
+            conn.commit()
+    else :
+        pass
+
