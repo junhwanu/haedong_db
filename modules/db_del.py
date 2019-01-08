4fd2719e435d447097ffac85ca6d1124fdcc4508
genioustar@gmail.com <4574gkdl>
2019-01-09 08:45:46 +0900

    임시저장

--- modules/db_del.py
+++ modules/db_del.py
@@ -5,7 +5,7 @@ import time
 cour=None
 conn=None
 
-conn = pymysql.connect(host='211.253.10.91',user='root',password='goehddl',db='haedong4',charset='utf8')
+conn = pymysql.connect(host='211.253.8.64',user='root',password='goehddl',db='haedong4',charset='utf8')
 
 #result = cour.execute('select * from GCG18 where working_day =\'2018-01-09\'')
 with conn.cursor() as cour :
@@ -14,13 +14,13 @@ with conn.cursor() as cour :
 # print(result)
 
 for i in result2 :
-    query = "select date from %s where working_day >=\'%s\'" % (i[0], str(datetime.date.today()))
+    query = "select date from %s where working_day >=\'%s\'" % (i[0], '2019-01-04')
     print(query)
     with conn.cursor() as cour:
         count = cour.execute(query)
         print(count)
     if count>0:
-        query1 = "delete from %s where working_day >= '%s'" % (i[0], str(datetime.date.today()))
+        query1 = "delete from %s where working_day >= '%s'" % (i[0], '2019-01-04')
         with conn.cursor() as cour:
             cour.execute(query1)
             print(query1)
